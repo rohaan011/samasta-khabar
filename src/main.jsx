f943cc91765e73f4cec907af453e125955f482a0
rohaan011 <rohanpzpt6@gmail.com>
2025-05-13 18:45:19 +0545

    admin page

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,21 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./index.css";
-import SignupPage from "./pages/SignupPage.jsx";
 import UserHomePage from "./pages/userHomePage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
 import ReporterHomePage from "./pages/reporterHomePage.jsx";
 import UserNewsArticle from "./pages/UserNewsArtice.jsx";
 import ReporterPostPage from "./pages/ReporterPostPage.jsx";
 import AdminApprovalPage from "./pages/AdminApprovalPage.jsx";
 import AdminHomepage from "./pages/AdminHomePage.jsx";
+import LoginPage from "./pages/loginPage.jsx";
+import SignupPage from "./pages/signupPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/user" element={<UserHomePage />} />
         <Route path="/reporter" element={<ReporterHomePage />} />
         <Route path="/usernewsarticle" element={<UserNewsArticle />} />
--- src/pages/signupPage.jsx
+++ src/pages/signupPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./styles/signupPage.css";
+import "./styles/SignupPage.css";
 
 const SignupPage = () => {
   const [formData, setFormData] = useState({
